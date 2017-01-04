b458a95d6d81483f2156c62f4089e224f1f81a28
Aman Vishnani <aman62@gmail.com>
2017-01-04 20:17:05 +0530

    Added Mongo Settings

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 var express = require('express');
 var app = new express();
 var mongoose = require('mongoose');
+
+app.set('MongoHost',process.env.OPENSHIFT_MONGODB_DB_HOST);
+app.set('MongoPort',parseInt(process.env.OPENSHIFT_MONGODB_DB_PORT));
+app.set('MongoUser',process.env.OPENSHIFT_MONGODB_DB_USERNAME);
+app.set('MongoPass',process.env.OPENSHIFT_MONGODB_DB_PASSWORD);
+app.set('DbName',process.env.OPENSHIFT_APP_NAME);
 app.set('port', parseInt(process.env.OPENSHIFT_NODEJS_PORT) || parseInt(process.env.PORT)|| 3000);
 app.set('ip', process.env.OPENSHIFT_NODEJS_IP ||process.env.IP|| '127.0.0.1');
 
 
+var mongoURL = 'mongodb://'+app.get('MongoUser')+':'+app.get('MongoPass')+'@'+app.get('MongoHost')+':'+app.get('MongoPort')+'/'+app.get('DbName');
 app.get('/', function (req, res) {
   res.send('Hello World!')
 });
