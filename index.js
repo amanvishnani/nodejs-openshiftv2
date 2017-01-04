1a28fdf86e38f547709c99f88093ca29d6bbd97b
Aman Vishnani <aman62@gmail.com>
2017-01-04 19:01:15 +0530

    Triggering the build

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = new express();
 var mongoose = require('mongoose');
-app.set('port', parseInt(process.env.OPENSHIFT_NODEJS_PORT) || 8080);
+app.set('port', parseInt(process.env.OPENSHIFT_NODEJS_PORT) || parseInt(process.env.PORT)|| 3000);
 
 
 app.get('/', function (req, res) {
