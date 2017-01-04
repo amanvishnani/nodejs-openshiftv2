79681a80c15bdb48ee085a6f614bf3a32d71b206
Aman Vishnani <aman62@gmail.com>
2017-01-04 20:18:12 +0530

    Added Mongo Settings

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.set('ip', process.env.OPENSHIFT_NODEJS_IP ||process.env.IP|| '127.0.0.1');
 
 var mongoURL = 'mongodb://'+app.get('MongoUser')+':'+app.get('MongoPass')+'@'+app.get('MongoHost')+':'+app.get('MongoPort')+'/'+app.get('DbName');
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+  res.send(mongoURL);
 });
 try{
     app.listen(app.get('port'), app.get('ip'),function () {
