5cb95dafa347c337b1b4732759af6570f016515b
Aman Vishnani <aman62@gmail.com>
2017-01-04 19:59:15 +0530

    Triggering the build

--- index.js
+++ index.js
@@ -8,7 +8,12 @@ app.set('ip', process.env.IP ||process.env.IP|| '127.0.0.1');
 app.get('/', function (req, res) {
   res.send('Hello World!')
 });
-
-app.listen(app.get('port'), app.get('ip'),function () {
+try{
+    app.listen(app.get('port'), app.get('ip'),function () {
   console.log('Example app listening on port '+app.get('port'));
 })
+}catch(e){
+    console.log(e);
+    console.error(e);
+}
+
